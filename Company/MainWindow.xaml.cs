c50be70fe96fe1991eae7bfba6be7be309efb81f
acxseles <bogachevalexandra@gmail.com>
2025-12-12 21:42:49 +0300

    работает имбулечка

--- Company/MainWindow.xaml.cs
+++ Company/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Company.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Company
@@ -9,34 +10,72 @@ namespace Company
     /// </summary>
     public partial class MainWindow : Window
     {
+
         AppDbContext dbContext = new();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Загружает данные из бд в DataGrid, отображая таблицу Employee с объединением таблицы Post
+        /// </summary>
+        /// <returns>
+        /// Возвращает список сотрудников из таблицы Employee и зарплату из Post
+        /// </returns>
         private async Task LoadData()
         {
             try
             {
-                ItemsDataGrid.ItemsSource = await dbContext.Employees
-                    .Include(e => e.Statuses)
-                    .ThenInclude(s => s.Post)
-                    .ToListAsync();
+                ItemsDataGrid.ItemsSource = await dbContext.Employees.Select(e => new
+                {
+                    e.Id,
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    Wages = e.Statuses.Where(s => s.EmployeeId == e.Id).Select(s => s.Post.Wages).FirstOrDefault()
+                }).ToListAsync();
             }
             catch
             {
                 MessageBox.Show("Нет подключения к базе данных");
             }
         }
-
+       
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
             await LoadData();
         }
 
+        /// <summary>
+        /// Фильтрация по ФИО, после нажатия на кнопку фильтруется таблица и возвращает отфильтрованные данные 
+        /// </summary>
         private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
+            //предполагает, что SearchBox содержит данные для фильтрации
+            var filter = SearchBox.Text.ToLower();
+            var filtered = dbContext.Employees.Where(e =>
+            e.FirstName.ToLower().Contains(filter) ||
+            e.LastName.ToLower().Contains(filter) ||
+            e.MiddleName.ToLower().Contains(filter));
 
+            ItemsDataGrid.ItemsSource = filtered.Select(e => new
+            {
+                e.Id,
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                Wages = e.Statuses.Where(s => s.EmployeeId == e.Id).Select(s => s.Post.Wages).FirstOrDefault()
+            }).ToList();
+        }
+
+        /// <summary>
+        /// сброс фильтрации
+        /// </summary>
+        private async void ResetButtun_Click(object sender, RoutedEventArgs e)
+        {
+            SearchBox.Text = "";
+            await LoadData();
         }
     }
 }
